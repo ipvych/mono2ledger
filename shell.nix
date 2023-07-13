ad6a4af1c7d1f9d315c10b2aca56b2ee30795d6a
Ivan Popovych <ivan@ipvych.com>
2023-07-13 18:59:56 +0300

    Switch from flakes to plain requirements.txt
    Needed pydantic, pydantic is broken in nix with some imports not being
    possible. Just use plain requirements.txt to not bother with
    maintaining that. In future I might remove requirements.txt as well in
    favor of just declaring dependencies in setup.py or whatever

--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  buildInputs = let
-    python = pkgs.python311;
-    ppkgs = ps: with ps; [ pycountry requests ];
-  in [(python.withPackages ppkgs)];
-}
