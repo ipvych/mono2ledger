bcbef21ba97fbc4d160f71f9168ff2f536f013d2
Ivan Popovych <ivan@ipvych.com>
2023-07-15 13:56:47 +0300

    Simplify cross-card statement detection and printing algorithm
    The idea is to use the fact that transactions are ordered properly to
    reduce the amount of backtracking that is done with previous
    find_source method by changing it to simple iteration. With this
    most of methods from classes can be moved to functions as well.
    
    I want to switch to using indexed loop which deletes elements from the
    list to be safe from cases where there is transaction and withdraw at
    the wrong times which current algo will not detect and probably
    mangle result due to use of iterator, but I want to keep the general
    workflow detection as is defined here and prefer it over previous
    find_source method.

--- mono2ledger/main.py
+++ mono2ledger/main.py
@@ -11,7 +11,7 @@ import time
 from datetime import datetime, timedelta
 from functools import cache
 from pathlib import Path
-from typing import Iterator, Optional, TextIO
+from typing import Optional, TextIO
 from urllib.error import HTTPError
 from urllib.parse import urljoin
 from urllib.request import Request, urlopen
@@ -24,12 +24,9 @@ import yaml
 from pycountry import currencies
 
 from .cli import err, info, warn
-from .config import ConfigModel
+from .config import ConfigModel, MatcherValue
 
 Currency = list(currencies)[0].__class__
-
-TRANSFER_MCC = 4829
-now = datetime.now()
 
 
 @cache
@@ -80,92 +77,29 @@ class JSONObject:
             return self.json["".join(items)]
         return self.json[item]
 
+    def get(self, item, default=None):
+        try:
+            return self.__getattr__(item)
+        except KeyError:
+            return default
+
     def __repr__(self):
         return str(self.json)
 
 
-class StatementItem(JSONObject):
-    account: "Account" = None
-
-    @property
-    def is_send(self):
-        return self.operation_amount < 0
-
-    @property
-    def is_receive(self):
-        return self.operation_amount > 0
-
-
 class Account(JSONObject):
-    _statements: Optional[list] = None
-
-    def fetch_statements(self, from_time: datetime, to_time: datetime):
-        if self._statements is not None:
-            raise ValueError(
-                "Statements are already fetched."
-                " Fetching multiple times with different dates is not supported"
-            )
-        intervals = []
-        if (to_time - from_time).days <= 30:
-            intervals.append([from_time, to_time])
-        else:
-            current_interval = to_time - from_time
-            current_time = from_time
-            # TODO: These is probably a nicer way to do this with recursion
-            while current_interval.days > 30:
-                # This can do requests with current timestampt in the future but API
-                # does not seem to care and neither do I
-                interval = current_time + timedelta(days=30)
-                intervals.append([current_time, interval])
-                current_interval -= 30
-                current_time = interval
-        self._statements = []
-        for from_time, to_time in intervals:
-            try:
-                response = fetch(
-                    "/personal/statement"
-                    f"/{self.id}"
-                    f"/{int(from_time.timestamp())}"
-                    f"/{int(to_time.timestamp())}"
-                )
-                # TODO: Better error handler - in particular throttling should be
-                # handled well
-            except HTTPError as e:
-                print(e.read().decode())
-                raise e
-            self._statements += [StatementItem(x, account=self) for x in response]
-
-    @property
-    def statements(self) -> list[StatementItem]:
-        if self._statements is None:
-            raise ValueError(
-                "You need to fetch statements by calling 'fetch_statements' first"
-            )
-        return self._statements
-
-    def filter_statements(
-        self, from_time: datetime = None, to_time: datetime = None, **kwargs
-    ) -> Iterator[StatementItem]:
-        if self._statements is None:
-            raise ValueError(
-                "You need to fetch statements by calling 'fetch_statements' first"
-            )
-
-        def matcher(x):
-            predicates: list[bool] = [
-                all(getattr(x, key) == value for key, value in kwargs.items())
-            ]
-            if from_time:
-                predicates.append(x.time >= from_time)
-            if to_time:
-                predicates.append(x.time <= to_time)
-
-            return all(predicates)
-
-        return filter(matcher, self._statements)
+    pass
 
 
-def get_last_transaction_date(file: TextIO) -> datetime:
+class StatementItem(JSONObject):
+    account = None
+
+    def __init__(self, json_data, account=None, **kwargs):
+        self.account = account
+        super().__init__(json_data, **kwargs)
+
+
+def get_last_transaction_date(file: TextIO, default=None) -> datetime:
     """
     Return date of the last ledger transaction in file.
     """
@@ -185,210 +119,210 @@ def get_last_transaction_date(file: TextIO) -> datetime:
             and (match := pattern.findall(line))
         ):
             result = match[0]
-    if not result:
-        warn(
-            "Could not get last transaction date from file. "
-            "Fetching transactions for last 30 days"
-        )
-        return now - timedelta(30)
 
-    return datetime.strptime(result, get_config().settings.ledger_date_format)
+    if result:
+        return datetime.strptime(result, get_config().settings.ledger_date_format)
+    return default
 
 
 def fetch_accounts() -> list[Account]:
+    with open("./clientinfo.json") as f:
+        response = json.load(f)["accounts"]
     return [
         Account(account)
-        for account in fetch("/personal/client-info")["accounts"]
+        for account in response  # fetch("/personal/client-info")["accounts"]
         if account["id"] not in get_config().settings.ignored_accounts
     ]
 
 
+def fetch_statements(
+    accounts: list[Account], from_time: datetime, to_time: datetime
+) -> list[StatementItem]:
+    intervals = []
+    if (to_time - from_time).days <= 30:
+        intervals.append((from_time, to_time))
+    else:
+        current_interval = to_time - from_time
+        current_time = from_time
+        # TODO: These is probably a nicer way to do this with recursion
+        while current_interval.days > 30:
+            # This can do requests with current timestampt in the future but API
+            # does not seem to care and neither do I
+            interval = current_time + timedelta(days=30)
+            intervals.append((current_time, interval))
+            current_interval -= 30
+            current_time = interval
+
+    datefmt = "%Y-%m-%d"
+    for account, interval in itertools.product(accounts, intervals):
+        from_time, to_time = interval
+        try:
+            # response = fetch(
+            #     "/personal/statement"
+            #     f"/{self.id}"
+            #     f"/{int(from_time.timestamp())}"
+            #     f"/{int(to_time.timestamp())}"
+            # )
+            with open("./statements.json") as f:
+                response = json.load(f)[account.id]
+            # TODO: Better error handler - in particular throttling should be
+            # handled well
+        except HTTPError as e:
+            print(e.read().decode())
+            raise e
+        # TODO: Need to sleep when there are multiple intervals to not hit rate
+        # limits
+        yield from (StatementItem(x, account=account) for x in response)
+        info(
+            f"Fetched statements for account {account.id}"
+            f" from {from_time.strftime(datefmt)} to {to_time.strftime(datefmt)}."
+            " Waiting 60 seconds to obey API rate limit"
+        )
+        # time.sleep(60)
+
+
+def get_ledger_account_for_account(account: Account) -> str:
+    # TODO: Default value should be configurable here, maybe, at the very least
+    # i would like to define const somewhere else
+    return get_config().match_account(account.id, f"Assets:Mono2ledger:{account.id}")
+
+
+def format_transaction(
+    date: datetime,
+    payee: str,
+    from_account: str,
+    to_account: str,
+    amount: float,
+    currency: str,
+    exchange_amount: float,
+    exchange_currency: str,
+) -> str:
+    config = get_config()
+
+    def format_amount(amount, pad=True):
+        if config.settings.trim_leading_zeroes and amount % 1 == 0:
+            return f"{int(amount):8}" if pad else str(int(amount))
+        return f"{int(amount):8.2f}" if pad else f"{int(amount):.2f}"
+
+    if amount < 0:
+        to_account, from_account = from_account, to_account
+        amount = -amount
+        if exchange_amount:
+            exchange_amount = -exchange_amount
+
+    exchange = (
+        f" @@ {format_amount(exchange_amount, pad=False)} {exchange_currency}"
+        if exchange_amount and exchange_currency
+        else ""
+    )
+
+    return (
+        f"{date.strftime(config.settings.ledger_date_format)} {payee}\n"
+        f"\t{to_account:60} {format_amount(amount)} {currency}"
+        f"{exchange}\n"
+        f"\t{from_account}\n"
+    )
+
+
+def format_ledger_transaction(
+    statement: StatementItem, source_statement: Optional[StatementItem] = None
+) -> str:
+    exchange_amount = None
+    exchange_currency = None
+    if source_statement:
+        payee = get_config().settings.transfer_payee
+        to_account = get_ledger_account_for_account(statement.account)
+        from_account = source_statement.account
+        amount = statement.amount
+        if source_statement.amount != source_statement.operation_amount:
+            exchange_amount = source_statement.amount / 100
+            exchange_currency = currencies.get(
+                numeric=str(source_statement.account.currency_code)
+            ).alpha_3
+    else:
+        match: MatcherValue = get_config().match_statement(statement)
+        payee = match.payee if match.payee else statement.description
+        from_account = statement.account
+        to_account = (
+            match.ledger_account
+            if match.ledger_account
+            else f"Expenses:Mono2ledger:{statement.account.id}:{statement.id}"
+        )
+        amount = -statement.amount
+        if statement.amount != statement.operation_amount:
+            exchange_amount = statement.amount / 100
+            exchange_currency = currencies.get(
+                numeric=str(statement.currency_code)
+            ).alpha_3
+
+    from_account = get_ledger_account_for_account(from_account)
+    currency = currencies.get(numeric=str(statement.currency_code)).alpha_3
+    return format_transaction(
+        datetime.fromtimestamp(statement.time),
+        payee,
+        from_account,
+        to_account,
+        amount / 100,
+        currency,
+        exchange_amount,
+        exchange_currency,
+    )
+
+
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog="mono2ledger")
     parser.add_argument("input", type=argparse.FileType("r"))
     parser.add_argument("output", type=argparse.FileType("w"), nargs="?")
     args = parser.parse_args(sys.argv[1:])
-    last_transaction_date = get_last_transaction_date(args.input)
+
+    now = datetime.now()
+    last_transaction_date = get_last_transaction_date(
+        args.input, now - timedelta(days=30)
+    )
+
     header_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
     header = f"\n;; Begin mono2ledger output\n;; Date and time: {header_datetime}\n"
     footer = ";; End mono2ledger output\n"
 
     accounts = fetch_accounts()
-    for account in accounts:
-        # TODO: Add indication that we are waiting. Just printing is kinda ugly
-        # ngl, I want something better than this for loop
-        account.fetch_statements(from_time=last_transaction_date, to_time=now)
-        print(f"Fetched statement for {account.id}. Waiting 60 seconds for throttle")
-        # TODO: Is there a cleaner way to do the same thing without accessing list?
-        if account != accounts[-1]:
-            time.sleep(60)
 
-    def get_matching_account(**kwargs):
-        return next(
-            filter(
-                lambda x: all(
-                    getattr(x, key) == value for key, value in kwargs.items()
-                ),
-                accounts,
-            ),
-            None,
-        )
+    def is_cross_card_statement(statement: StatementItem) -> bool:
+        # 4829 seems to be the MCC mono uses for card transfers
+        if statement.mcc != 4829:
+            return False
+        if counter_iban := statement.get("counter_iban"):
+            return counter_iban in (x.iban for x in accounts)
 
-    def guess_account(statement):
         description = statement.description
-        # TODO: Just use conunterIban instead of coming up with new names
-        if statement.recepient_iban:
-            return get_matching_account(iban=statement.recepient_iban)
-        if "ФОП" in description:
-            card_type = "fop"
-        elif matches := re.match(r".*(чорн|біл)(у|ої).*", description):
-            matched_card = matches[1]
-            if matched_card.startswith("чорн"):
-                card_type = "black"
-            elif matched_card.startswith("біл"):
-                card_type = "white"
-
-        if matches := re.match(r".*(гривне|євро|долар)(вий|вого).*", description):
-            matched_currency = matches[1]
-            currency = None
-            if matched_currency.startswith("гривн"):
-                currency = currencies.get(alpha_3="UAH")
-            elif matched_currency.startswith("євро"):
-                currency = currencies.get(alpha_3="EUR")
-            elif matched_currency.startswith("долар"):
-                currency = currencies.get(alpha_3="USD")
-            if currency:
-                return get_matching_account(currency=currency, type=card_type)
-
-    def format_ledger_transaction(statement, source_statement=None):
-        # TODO: get_account whihc will return account funds came from based on
-        # account, maybe from config
-        # TODO: get_to_account which will return appropriate account for expense
-        # transaction. Make sure to take into account that there are not only expenses
-
-        config = get_config()
-
-        def int_if_no_decimal(num: float) -> int | float:
-            """
-            Convert num to integer if it's decimal point == 0 else return it unmodified
-            """
-            return int(num) if num % 1 == 0.0 else num
-
-        exchange_amount = None
-        exchange_currency = None
-        if source_statement:
-            payee = config["transfer_payee"]
-            to_account = config["account"][statement.account.id]["name"]
-            from_account = config["account"][source_statement.account.id]["name"]
-            amount = statement.amount
-            if source_statement.amount != source_statement.operation_amount:
-                exchange_amount = source_statement.amount
-                exchange_currency = source_statement.account.currency
-        else:
-            payee = f"{statement.description}"
-            from_account = config["account"][statement.account.id]["name"]
-            to_account = f"statement_id:{statement.id}"
-            amount = -statement.amount
-            if statement.amount != statement.operation_amount:
-                exchange_amount = statement.amount
-                exchange_currency = statement.currency
-
-        if statement.is_receive:
-            to_account, from_account = from_account, to_account
-            amount = -amount
-            exchange_amount = -exchange_amount
-
-        exchange = (
-            f" @@ {-exchange_amount} {exchange_currency.alpha_3}"
-            if exchange_amount and exchange_currency
-            else ""
-        )
-
-        # TODO: Trim zero does not have signed digits
-        # TODO: When the transaction is income swap recepiend and from account
-        return (
-            f"{statement.time.strftime(config.settings.ledger_date_format)} {payee}\n"
-            f"\t{to_account:60} {amount:8} {statement.account.currency.alpha_3}"
-            f"{exchange}\n"
-            f"\t{from_account}\n"
-        )
-
-    def find_source(statement):
-        if statement.mcc == TRANSFER_MCC:
-            if statement.is_send:
-                filters = {"from_time": statement.time - timedelta(minutes=5)}
-            else:
-                filters = {"to_time": statement.time + timedelta(minutes=5)}
-
-            filters |= {
-                "mcc": TRANSFER_MCC,
-                "currency": statement.currency,
-                "operation_amount": -statement.operation_amount,
-            }
-
-            if account := (
-                statement.account if statement.is_send else guess_account(statement)
-            ):
-                matched_statement = next(
-                    account.filter_statements(**filters), statement
-                )
-                if matched_statement != statement:
-                    return find_source(matched_statement)
-        return statement
-
-    def get_source(statement):
-        if statement.mcc == TRANSFER_MCC and (account := guess_account(statement)):
-            return find_source(statement, account)
-        return statement
+        has_card_type = False
+        has_currency = False
+        if "ФОП" in description or re.match(r".*(чорн|біл)(у|ої).*", description):
+            has_card_type = True
+        if re.match(r".*(гривне|євро|долар)(вий|вого).*", description):
+            has_currency = True
+        return has_card_type or has_currency
 
     statements = sorted(
-        (statement for account in accounts for statement in account.statements),
+        fetch_statements(accounts, last_transaction_date, now),
         key=lambda x: (x.time, x.amount),
+        reverse=True,
     )
-    cross_card_statements = []
-    normal_statements = []
 
-    for statement in statements:
-        account = guess_account(statement)
-        source = find_source(statement)
-        if source != statement:
-            # Due to the way statements are sorted we know that the next matching
-            # statement is the destination, and the source is always the same
-            if result := next(
-                filter(
-                    lambda x: source in x[1] or statement in x[1],
-                    enumerate(cross_card_statements),
-                ),
-                None,
-            ):
-                index = result[0]
-                cross_card_statements[index].append(statement)
-            else:
-                cross_card_statements.append([statement, source])
-        elif not account:
-            normal_statements.append(statement)
+    statements = iter(statements)
 
-    all_statements = sorted(
-        itertools.chain(cross_card_statements, normal_statements),
-        key=(
-            lambda x: (x.time, x.amount)
-            if not isinstance(x, list)
-            else (x[0].time, x[0].amount)
-        ),
-    )
     print(header)
-
-    for statement in all_statements:
-        if isinstance(statement, list):
-            sorted_cross_card_statement = sorted(
-                statement, key=lambda x: (x.time, x.amount), reverse=True
-            )
-            print(
-                format_ledger_transaction(
-                    sorted_cross_card_statement[0], sorted_cross_card_statement[-1]
-                )
-            )
-            pass
+    for statement in statements:
+        if is_cross_card_statement(statement):
+            current_statement = statement
+            while (
+                (next_statement := next(statements, None))
+                and current_statement.operation_amount
+                == -next_statement.operation_amount
+                and current_statement.currency_code == next_statement.currency_code
+                and current_statement.mcc == next_statement.mcc
+            ):
+                current_statement = next_statement
+            print(format_ledger_transaction(statement, current_statement))
         else:
             print(format_ledger_transaction(statement))
 
